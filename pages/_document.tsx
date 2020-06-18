44ebf4c666df61cfa7cec55ac7c615d9040c082f
Samson SSali <jitpomi@gmail.com>
2020-06-17 20:56:51 -0700

    local images optimized

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from 'theme';
 
+
 export default class MyDocument extends Document {
   render() {
     return (
@@ -53,12 +54,14 @@ MyDocument.getInitialProps = async (ctx) => {
   const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
 
+
   ctx.renderPage = () =>
     originalRenderPage({
       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     });
 
   const initialProps = await Document.getInitialProps(ctx);
+
 
   return {
     ...initialProps,
--- pages/lessons/setup.tsx
+++ pages/lessons/setup.tsx
@@ -1,9 +1,11 @@
-import { Button, Typography, makeStyles } from "@material-ui/core";
+import { Button, Typography, makeStyles, Box } from "@material-ui/core";
 import Link from 'next/link'
+
 
 const useStyles = makeStyles({
     heloThereStyle:{
-        fontStyle:'oblique'
+        fontStyle:'oblique',
+        fontSize:'5rem'
     }
 })
 
@@ -13,11 +15,35 @@ export default function Learn(){
 
     return(
        <div>
-       <Typography color='secondary' variant='h1' className={classes.heloThereStyle}>Hello There</Typography>
-       <Button color='primary' variant='contained'>I want to see this </Button>
-     <Link href='/'>
-     <Button color='primary' variant='outlined'>Back home </Button>
-     </Link>
+       
+     <div className="image-container">
+      <img className="blur-image" src={require(`public/africa.jpg?lqip`)} />
+      <img src={require(`public/africa.jpg?webp`)} />
+    </div>
+   <Box margin='1rem'>
+   <Typography color='secondary' variant='h1' className={classes.heloThereStyle}>Hello There</Typography>
+   <Button color='primary' variant='contained'>I want to see this </Button>
+ <Link href='/'>
+ <Button color='primary' variant='outlined'>Back home </Button>
+ </Link>
+   </Box>
+    <style jsx>{`
+        .image-container: {
+          position: relative:
+        }
+        .blur-image img {
+          blur(7rem);
+          width: 30%;
+          height: 20%;
+        }
+        img {
+          position: absolute;
+          width: 30%;
+          height: 20%;
+          top: 0;
+          right: 0;
+        }
+    `}</style>
        </div>
     )
 }
